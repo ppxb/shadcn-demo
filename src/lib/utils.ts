536e684d9b442ef1ff2dc6f8eebe493d5d06d617
ppxb <317842449@qq.com>
2026-04-15 11:10:35 +0800

    chore: add fumadocs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function mergeRefs<T>(...refs: (React.Ref<T> | undefined)[]): React.RefCallback<T> {
+  return value => {
+    refs.forEach(ref => {
+      if (typeof ref === 'function') {
+        ref(value)
+      } else if (ref) {
+        ref.current = value
+      }
+    })
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import '@/styles/globals.css'
 import App from './App.tsx'
+import ChangelogPage from './pages/changelog.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ChangelogPage />
   </StrictMode>
 )
